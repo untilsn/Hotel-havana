3940a843665b4501ac0d500293e364cb541f31ed
untilsN <120695452+untilsn@users.noreply.github.com>
2023-05-28 17:39:37 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -54,25 +54,12 @@ openBtn.forEach((item) => {
   });
 });
 
-// const toTop = document.querySelector(".scroll-top i");
+const scrolltopBtn = document.querySelector(".scroll-top");
 
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (
-    document.body.scrollTop > 920 ||
-    document.documentElement.scrollTop > 920
-  ) {
-    document.querySelector(".scroll-top i").style.display = "block";
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    scrolltopBtn.classList.add("active");
   } else {
-    document.querySelector(".scroll-top i").style.display = "none";
+    scrolltopBtn.classList.remove("active");
   }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+});
